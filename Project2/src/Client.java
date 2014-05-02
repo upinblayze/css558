25036a8c3f9e6080bd28c15604a2b7102c207264
binrusas@uw.edu <binrusas@uw.edu@2ff96f0c-952c-983c-e3f3-b91e1aa7a809>
2014-05-02 06:12:40 +0000

    updated

--- Project2/src/Client.java
+++ Project2/src/Client.java
@@ -12,11 +12,46 @@ public class Client
 {
 	private Logger logger ;
 	private Server server;
-	public Client(Server server) throws IOException {
+	private String clientID;
+	public Client(Server server, String clientID) throws IOException {
 		this.server = server;
+		this.clientID = clientID;
 		String timestamp = Logger.getTimestamp();
-		logger = new Logger("client-" + timestamp + ".log");
-		logger.log("Client is running on : " + Inet4Address.getLocalHost(), true);
+		logger = new Logger("Client-" + clientID +"-" + timestamp + ".log");
+		logger.log("Client " + clientID + " is running on : " + Inet4Address.getLocalHost(), true);
+	}
+
+	public void put(String key, String value){
+		logger.log("Client " + clientID + " has issued request: put("+key+","+value+")" , true);
+		try {
+			this.server.put(key, value);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void get(String key){
+		logger.log("Client " + clientID + " has issued request: get("+key+")" , true);
+		String value = null;
+		try {
+			value = this.server.get(key);
+			logger.log("Client " + clientID + " has got--> key = " + key+ " , value = " + value, true);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void delete(String key){
+		logger.log("Client " + clientID + " has issued request: delete("+key+")" , true);
+		try {
+			this.server.delete(key);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public void prompt() throws RemoteException {
@@ -28,22 +63,17 @@ public class Client
 			if(request.length >= 2){
 				if(request[0].toLowerCase().equals("put")){
 					if(request.length == 3){
-						logger.log("Request: " + request[0].toLowerCase() + " (" + request[1] + "," + request[2] + ")", true);
-						server.put(request[1], request[2]);
+						put(request[1],request[2]);
 					}
 					else{
 						System.out.println("Invalid request");
 					}
 				}
 				else if(request[0].toLowerCase().equals("get")){
-					logger.log("Request: " + request[0].toLowerCase() + " (" + request[1] + ")", true);
-					String value = server.get(request[1]);
-					logger.log("key = " + request[1] + " , value = " + value, true);
-
+					get(request[1]);
 				}
 				else if(request[0].toLowerCase().equals("delete")){
-					logger.log("Request: " + request[0].toLowerCase() + " (" + request[1] + ")", true);
-					server.delete(request[1]);
+					delete(request[1]);
 				}
 			}
 			else if(request.length == 1 && request[0].toLowerCase().equals("exit")){
@@ -55,19 +85,77 @@ public class Client
 			}
 		}
 	}
-	public static void main(String []args)
-	{
+	public static void main(String []args){
 		if(args.length < 2){
 			System.out.println("Error: no enough parameters");
 			System.exit(2);
 		}
 		String serverName = args[0];
 		int serverPort = Integer.parseInt(args[1]);
+		final String url = "rmi://localhost:" + serverPort + "/" + serverName;
 		try {
-			String name = "rmi://localhost:" + serverPort + "/" + serverName;
-			Server server = (Server) Naming.lookup(name);
-			Client client = new Client(server);
-			client.prompt();
+			
+			// client 1
+			new Thread(
+					new Runnable() {
+						public void run() {
+							Server server = null;
+							try {
+								server = (Server) Naming.lookup(url);
+								Client client = new Client(server , "1");
+								client.delete("pink");
+								client.put("black", "2525");
+								client.delete("yellow");
+								client.put("red", "7548");
+								client.put("green", "5555");
+								client.get("black");
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}).start();
+			
+			//client 2
+			new Thread(
+					new Runnable() {
+						public void run() {
+							Server server = null;
+							try {
+								server = (Server) Naming.lookup(url);
+								Client client = new Client(server , "2");
+								client.put("yellow", "8888");
+								client.get("red");
+								client.delete("black");
+								client.get("yellow");
+								
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}).start();
+			
+			//client 3
+			new Thread(
+					new Runnable() {
+						public void run() {
+							Server server = null;
+							try {
+								server = (Server) Naming.lookup(url);
+								Client client = new Client(server, "3");
+								client.put("pink", "1144");
+								client.delete("green");
+								client.put("orang", "2233");
+								client.get("pink");
+								client.put("brown", "8965");
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}).start();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
