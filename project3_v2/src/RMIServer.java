5be420ce22069f4451f0e0d250da4ff663106a71
kellec2@uw.edu <kellec2@uw.edu@2ff96f0c-952c-983c-e3f3-b91e1aa7a809>
2014-05-24 22:56:10 +0000

    Node connection issue resolved

--- project3_v2/src/RMIServer.java
+++ project3_v2/src/RMIServer.java
@@ -26,38 +26,46 @@ public class RMIServer {
 			KVStore kvs = new KVStore();
 			KVService stub = (KVService)UnicastRemoteObject.exportObject(kvs,0);
 			Registry reg = LocateRegistry.createRegistry(1099);
-			reg.rebind(name, stub);
+			reg.bind(name, stub);
 
 			String name2 = "KVSync";
 			RMItwophasecommit stub2 = (RMItwophasecommit)UnicastRemoteObject.toStub(kvs);
 			Registry reg2 = LocateRegistry.createRegistry(1098);
-			reg2.rebind(name2, stub2);
+			reg.bind(name2, stub2);
 
 			List<RMItwophasecommit> my_replicated_servers = new ArrayList<RMItwophasecommit>();
 
 			int i = 0;
 			ArrayList<String> missingServers = new ArrayList<String>(Arrays.asList(args));
+			System.out.println(args[0]);
+			System.out.println(missingServers.size());
+			for(String s:missingServers){
+				System.out.println(s);
+			}
 			RMItwophasecommit tpc;
 			while(!missingServers.isEmpty()){
+				System.out.println("Trying server: " +missingServers.get(0));
 				try{
-				tpc = (RMItwophasecommit) Naming.lookup("//"+ missingServers.get(i) +"/"+name2);
-				if(tpc != null){
-					my_replicated_servers.add(tpc);
-					missingServers.remove(i);
-					System.out.println(missingServers.isEmpty());
-				}
+					tpc = (RMItwophasecommit) Naming.lookup("//"+ missingServers.get(0) +"/"+name2);
+					if(tpc != null){
+						my_replicated_servers.add(tpc);
+						missingServers.remove(i);
+						System.out.println(missingServers.isEmpty());
+
+					}
 				}catch(Exception ex){
-					System.out.println("Failed to connect to " + missingServers.get(i));
+					System.out.println("Failed to connect to " + missingServers.get(0));
 				}
-				i++;
-				i = i % (args.length);
+//				i++;
+//				i = i % (missingServers.size());
 			}
 
 			kvs.setMy_replicated_servers(my_replicated_servers);
 
 			System.out.println("Server ready");
 		}catch(Exception e){
-			System.out.println("RMIServer error: "+e.getMessage());
+//			System.out.println("RMIServer error: "+e.getMessage());
+			e.printStackTrace();
 			System.exit(0);
 		}
 	}
