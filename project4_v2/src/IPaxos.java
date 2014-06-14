e089825e8c59b401458af59c72c34f0ae2d8d7b0
kellec2@uw.edu <kellec2@uw.edu@2ff96f0c-952c-983c-e3f3-b91e1aa7a809>
2014-06-14 21:01:42 +0000

    updated IPaxos to work as needed

--- project4_v2/src/IPaxos.java
+++ project4_v2/src/IPaxos.java
@@ -1,48 +1,12 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 
 public interface IPaxos extends Remote {
 	
-	/**
-	 * An other server will call this method on a remote object, passing in its
-	 * own server id.   If the server id is higher that the local host's, then
-	 * return ACK, NACK otherwise.
-	 * @param server_id
-	 * @return
-	 */
-	String checkForLeader(int server_id);
 	
-	/**
-	 *This method receives the slot number that the proposer is trying
-	 *pass a proposal for.  If it already accepted a proposal for that slot,
-	 *then it returns that proposal to the proposer.  If the proposer, has not
-	 *accepted anything for this slot, then this acceptor returns an ACK
-	 *message.
-	 * @param n - is the proposals index number
-	 * @return - the accepted value, o.w. return ACK
-	 */
-	String prepare(int n);
+	public String[] prepare(float n) throws RemoteException;
 	
-	
-	/**
-	 * The proposer calls accept on remote objects, passing the index slot 
-	 * number and the request to be accepted.  
-	 * @param n - index slot number
-	 * @param value - the command to be accepted
-	 * @return - the index of the first unchosen command or empty slot that is 
-	 * 				less than n, o.w. return ACK
-	 */
-	String accept(int n, String value);
-	
-	/**
-	 * When the proposal receives an index from an acceptor for its first
-	 * unknown chosen array from the result of the accept method or success
-	 * method, it calls this method on the remote object, passing in the 
-	 * chosen command and the commands index in the replicated log.
-	 * @param index
-	 * @param value
-	 * @return
-	 */
-	String success(int index, String value);
+	public int accept(float n, String value) throws RemoteException;
 	
 }
